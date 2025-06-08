6a45df3b51b95ca2697007bdc29918e03a2d9df5
Kunal Kudande <kudandekunal1410@gmail.com>
2025-06-08 15:21:06 +0530

    Added Vercel

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Import routers
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/history", require("./controllers/historyController"));
 app.use("/api/image", require("./routes/image"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// For local run
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+// Required by Vercel
+module.exports = app;
